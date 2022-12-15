afc88b1039ca01ba6432a7c0b460fba33258f6cf
Oscar Cifuentes <oscarcifuentescisterna@gmail.com>
2022-12-15 17:44:08 -0300

    Add files via upload

---
+++ asset/js/correo.js
@@ -0,0 +1,6 @@
+// Envíame un Correo
+function sendEmailDefault(){
+    var email = "ocifuentes@netsecurity.cl";
+    var subject = "Contacto";
+    window.open(`mailto:${email}?subject=${subject}`);
+  }
---
+++ asset/js/saludo.js
@@ -0,0 +1,4 @@
+// Nombre
+let nombre = "";
+nombre = prompt("¿Cual es tu nombre?");
+alert("Hola " + nombre + ", Bienvenid@ a mi página web");
